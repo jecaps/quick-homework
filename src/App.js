701703e17896e0c3de1f085a1c7150d6a886d76f
Jerika Kanz <jerikacapila@gmail.com>
2022-09-29 21:10:49 +0200

    Add and fetch api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import { persons } from "./assets/persons";
+// import { persons } from "./assets/persons";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+import Form from "./components/Form";
 import Person from "./components/Person";
 
 function App() {
+  const [persons, setPersons] = useState([]);
+  const [status, setStatus] = useState("");
+  useEffect(() => {
+    async function getPersons() {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/?status=${status}`
+      );
+
+      const data = await response.json();
+      console.log(data);
+      setPersons(data.results);
+    }
+    getPersons();
+  }, [status]);
+
   const personElement = persons.map((person) => {
     return (
       <Person
@@ -15,7 +32,12 @@ function App() {
     );
   });
 
-  return <ul>{personElement}</ul>;
+  return (
+    <>
+      <Form setStatus={setStatus} />
+      <ul>{personElement}</ul>
+    </>
+  );
 }
 
 export default App;
